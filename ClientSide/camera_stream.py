1ad443a6e93e4ba0587f6672fb7f164f210ae73f
phbruemmer <phbruemmer@gmail.com>
2024-07-28 18:59:29 +0200

    - client changes

--- ClientSide/camera_stream.py
+++ ClientSide/camera_stream.py
@@ -9,11 +9,6 @@ CAMERA_STREAM_STOP_EVENT = threading.Event()
 
 def start_camera_stream(sock):
     camera = cv2.VideoCapture(0)
-    resolution_x = capture_cam.view(camera).shape[0]
-    resolution_y = capture_cam.view(camera).shape[1]
-    print(resolution_x)
-    print(resolution_y)
-    sock.send(pickle.dumps([resolution_x, resolution_y]))
 
     while not CAMERA_STREAM_STOP_EVENT.is_set():
         frame = capture_cam.view(camera)
--- capture/capture_cam.py
+++ capture/capture_cam.py
@@ -3,10 +3,6 @@ import sys
 
 
 def view(video):
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
     ret, frame = video.read()
     return frame
 

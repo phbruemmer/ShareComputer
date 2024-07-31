b66a56b00601d29041bd286ef9d3b685df1e43a9
phbruemmer <phbruemmer@gmail.com>
2024-07-31 20:56:56 +0200

    - added better error handling

--- ClientSide/streams/stream_data.py
+++ ClientSide/streams/stream_data.py
@@ -4,11 +4,15 @@ import struct
 
 
 def send_data(sock, data):
+    error = None
     try:
         data_ = pickle.dumps(data)
         msg = struct.pack('Q', len(data_)) + data_
         sock.send(msg)
     except socket.error as e:
         print(f"[ERROR] - socket error - {e}")
+        error = True
     except Exception as e:
         print(f"[ERROR] - unexpected error - {e}")
+        error = True
+    return error
